4f62b5d9151c5447750911626e09b1302e8687aa
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-03-13 00:18:55 -0700

    improvements + bug fixes

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ACCEPTING_CHARS = "💀🪑"
+ACCEPTING_CHARS = ("💀", "🪑")
+
 
 class ChannelNotSetError(Exception):
     pass
@@ -15,32 +16,49 @@ class ChannelNotSetError(Exception):
 class Log:
     def __init__(self):
         self.log = []
+        self.last = None
         self.channel_id = None
-
-    def check(self, message):
-        if self.channel_id is None:
-            raise ChannelNotSetError
-        return self.channel_id == message.channel.id
 
     def add(self, message):
         self.log.append(message)
+        self.last = message
         print(f"Added message to log: {message.content}")
+
+    def pop(self):
+        self.log.pop()
 
     def clear(self):
         self.log = []
+        self.last = None
 
     def set_channel(self, channel_id):
         self.channel_id = channel_id
+
+    def flip(self):
+        self.log = list(reversed(self.log))
+
+    def query_counts(self, count):
+        counts = {}
+
+        for message in self.log:
+            if message.author not in counts:
+                counts[message.author] = 0
+            counts[message.author] += 1
+
+        return counts
+    
+    def query_user(self, user):
+        return [message for message in self.log if message.author == user]
+
+    def query_count(self, count):
+        return self.log[-count:]
 
     def __repr__(self):
         return "\n".join([message.content for message in self.log])
 
 
-def on_ready_wrapper(bot):
-    async def on_ready():
-        print(f"{bot.user.name} is ready")
-
-    return on_ready
+async def on_ready():
+    print(f"{bot.user.name} is ready")
 
 
 async def on_guild_join(guild):
@@ -52,52 +70,74 @@ async def on_guild_remove(guild):
 
 
 async def on_message(message):
-    if LOG.check(message):
-        print(f"Logged message from {message.author.name}")
-        print(f"Content: {message.content}")
-    else:
-        print(f"Message from {message.author.name} not logged")
-        print(f"Content: {message.content}")
+    if message.channel.id == LOG.channel_id:
+        if (message.content not in ACCEPTING_CHARS) or (
+            LOG.log and message.author == LOG.log[-1].author
+        ):
+            try:
+                await message.delete()
+            except discord.errors.NotFound:
+                print("Message not found")
+        else:
+            LOG.add(message)
 
 
 async def log(
     ctx,
-    count: Option(int, "Number of lines to show", required=False),
-    from_: Option(str, "Time to start from", required=False),
-    user: Option(str, "User to show logs for", required=False),
+    count: Option(int, "Number of lines to show", default=10, required=True),
+    user: Option(discord.User, "User to show logs for", required=False),
 ):
-    if from_ is not None:
-        if user is not None:
-            pass
-        await ctx.respond(f"from_ is {from_}")
-    elif count is not None:
-        if user is not None:
-            pass
-        await ctx.respond(f"count is {count}")
-    elif user is not None:
-        await ctx.respond(f"user is {user}")
+    await ctx.response.defer()
+
+    logs = []
+    if user is not None:
+        logs = LOG.query_user(user)[-count:]
     else:
-        if str(LOG) == "":
-            await ctx.respond("No logs")
-        else:
-            await ctx.respond(str(LOG))
+        logs = LOG.query_count(count)
+
+    # char limit: 2000
+    strings = []
+    curr = ""
+    for log in logs:
+        curr
+
+    for string in strings:
+        await ctx.followup.send(string)
 
 
 async def start_monitoring(ctx):
     await ctx.response.defer()
     LOG.clear()
-    print(ctx.channel_id)
     LOG.set_channel(ctx.channel_id)
 
+    last = None
+
     async for message in ctx.channel.history(limit=None):
-        await on_message(message)
+        if message.content not in ACCEPTING_CHARS:
+            try:
+                await message.delete()
+            except discord.errors.NotFound:
+                print("Message not found")
+            continue
+
+        if last is not None and message.author == last.author:
+            try:
+                await last.delete()
+                LOG.pop()
+            except discord.errors.NotFound:
+                print("Message not found")
+
+        LOG.add(message)
+        last = message
+
+    LOG.flip()
 
     await ctx.followup.send(f"Started monitoring {ctx.channel.name}")
 
 
 async def help(ctx):
     await ctx.respond(
-"""
+        """
 Commands:
 - /help: Get help with the bot
 - /log: Check the log
@@ -108,7 +148,8 @@ Commands:
 /log [count]: Check the last [count] lines of the log
 /log [from]: Check the log from [from] to now
 /log [user]: Check the log for [user]
-""")
+"""
+    )
 
 
 if __name__ == "__main__":
@@ -123,7 +164,7 @@ if __name__ == "__main__":
 
     LOG = Log()
 
-    bot.event(on_ready_wrapper(bot))
+    bot.event(on_ready)
     bot.event(on_guild_join)
     bot.event(on_guild_remove)
     bot.event(on_message)
@@ -135,4 +176,3 @@ if __name__ == "__main__":
     )(start_monitoring)
 
     bot.run(os.getenv("DISCORD_TOKEN"))
-
